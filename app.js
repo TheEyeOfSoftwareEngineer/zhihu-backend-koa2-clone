5f718f14001d54484482120a16f7442d5c6c6718
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-01-26 00:46:35 +1100

    update error handler

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const Koa = require('koa')
 const app = new Koa()
 const json = require('koa-json')
-const onerror = require('koa-onerror')
+const onerror = require('koa-json-error')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
 const registerRoutes = require('./routes')
 
 // error handler
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch(err) {
-    ctx.status = err.status || err.statusCode || 500
-    ctx.body = {
-      message: err.message
-    }
+app.use(onerror({
+  postFormat: (e, {stack, ...rest}) => {
+    process.env.NODE_ENV === 'production' 
+    ? rest : {stack, ...rest} // 在生产环境我们不返回stack的信息而在开发环境中返回所有信息
   }
-})
-onerror(app)
+})) // json化返回信息的处理库
 
 // middlewares
 app.use(bodyparser({
