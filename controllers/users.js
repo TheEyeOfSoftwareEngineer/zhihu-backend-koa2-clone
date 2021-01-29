d0fcb1f4559a74afb32d77dcd7fd1225492bb626
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-01-29 17:54:11 +1100

    add user profile

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const jwt = require('koa-jwt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/users')
 const {CONF} = require('../conf/conf')
 
@@ -9,7 +9,9 @@ class UserController {
   }
 
   async findById(ctx) {
-    const user = await User.findById(ctx.params.id)
+    const {fields} = ctx.query
+    const selectFileds = fields.split(';').filter(f=>f).map(f=> " +"+f).join('')
+    const user = await User.findById(ctx.params.id).select(selectFileds)
     if(!user) { 
       ctx.throw(404, '该用户不存在')       
     }
@@ -31,7 +33,14 @@ class UserController {
   async update(ctx) {
     ctx.verifyParams({
       name: {type: 'string', required: false},
-      password: {type: 'string', required: false}
+      password: {type: 'string', required: false},
+      avatar_url: {type: 'string', required: false},
+      gender: {type: 'string', required: false},
+      headline: {type: 'string', required: false},
+      locations: {type: 'array', itemType: 'string', required: false},
+      business: {type: 'string', required: false},
+      employments: {type:'array', itemType: 'object', required: false},
+      educations: {type:'array', itemType: 'object', required: false},
     })
     const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
     if(!user) {
