74011cee6bdc4e1e990fa913d6b0f6fdac8f8f89
TheEyeOfSoftwareEngineer <willjarvin@gmail.com>
2021-01-29 18:41:29 +1100

    add following and follower

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,8 @@ const { auth } = require('../middlewares/auth')
 
 const {
   find, findById, create, update, delete: del,
-  login, checkUser
+  login, checkUser, listFollowing, follow, unfollow,
+  listFollower
 } = require('../controllers/users')
 
 router.get('/', find)
@@ -14,7 +15,10 @@ router.get('/:id', findById)
 // PUT整体替换 PATCH替换一部分属性
 router.patch('/:id', auth, checkUser, update)
 router.delete('/:id', auth, checkUser, del)
-
 router.post('/login', login)
+router.get('/:id/following', listFollowing)
+router.get('/:id/listfollower', listFollower)
+router.put('/following/:id', auth, follow)
+router.delete('/following/:id', auth, unfollow)
 
 module.exports = router
